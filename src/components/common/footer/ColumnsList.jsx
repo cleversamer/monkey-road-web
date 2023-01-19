bec6fc6363bfd43fc6b41f1947b98b30e1d0d1e2
cleversamerr <thedev.samer@gmail.com>
2023-01-19 17:38:19 +0200

    fixed a bug in footetr

---
+++ src/components/common/footer/ColumnsList.jsx
@@ -0,0 +1,140 @@
+import styled from "styled-components";
+import { Link as RouterLink } from "react-router-dom";
+import { Link as ScrollLink, animateScroll as scroll } from "react-scroll";
+import { ROUTES } from "client";
+
+const ColumnsList = () => {
+  return (
+    <Container>
+      <ColumnItem>
+        <ColumnTitle>Website</ColumnTitle>
+
+        <ColumnRoute
+          to={ROUTES.CLIENT.HOME}
+          onClick={() => scroll.scrollToTop()}
+        >
+          Home
+        </ColumnRoute>
+
+        <ColumnRoute to={ROUTES.CLIENT.RENT_CARS}>Cars for rent</ColumnRoute>
+
+        <ColumnRoute to={ROUTES.CLIENT.PURCHASE_CARS}>
+          Cars for sale
+        </ColumnRoute>
+      </ColumnItem>
+
+      <ColumnItem>
+        <ColumnTitle>About</ColumnTitle>
+        <ColumnScrollLink
+          to="about-us"
+          smooth={true}
+          duration={500}
+          spy={true}
+          exact="true"
+        >
+          About us
+        </ColumnScrollLink>
+
+        <ColumnRoute to={ROUTES.CLIENT.HOME}>FAQ</ColumnRoute>
+
+        <ColumnRoute to={ROUTES.CLIENT.HOME}>Privacy policy</ColumnRoute>
+
+        <ColumnRoute to={ROUTES.CLIENT.HOME}>Terms of condition</ColumnRoute>
+      </ColumnItem>
+
+      <ColumnItem>
+        <ColumnTitle>App</ColumnTitle>
+
+        <ColumnLink target="_blank" href="https://play.google.com">
+          <StoreImage
+            src="/assets/icons/footer/app-store.svg"
+            alt="app store"
+          />
+        </ColumnLink>
+
+        <ColumnLink target="_blank" href="https://play.google.com">
+          <StoreImage
+            src="/assets/icons/footer/play-store.svg"
+            alt="play store"
+          />
+        </ColumnLink>
+      </ColumnItem>
+    </Container>
+  );
+};
+
+const Container = styled.ul`
+  list-style: none;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+
+  @media screen and (max-width: 1280px) {
+    order: 1;
+    width: 100%;
+    max-width: 600px;
+  }
+
+  @media screen and (max-width: 540px) {
+    grid-template-columns: repeat(auto-fit, minmax(160px, 1fr));
+    grid-row-gap: 30px;
+  }
+`;
+
+const ColumnItem = styled.li`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  color: #fff;
+  width: 250px;
+
+  @media screen and (max-width: 1280px) {
+    align-items: center;
+    justify-content: flex-start;
+    width: 100%;
+  }
+`;
+
+const ColumnTitle = styled.h6`
+  color: #fff;
+  font-size: 20px;
+  font-weight: 600;
+`;
+
+const ColumnRoute = styled(RouterLink)`
+  font-size: 15px;
+  width: fit-content;
+  text-transform: capitalize;
+  transition-duration: 176ms;
+
+  :hover {
+    color: #fe7777;
+  }
+`;
+
+const ColumnScrollLink = styled(ScrollLink)`
+  font-size: 15px;
+  width: fit-content;
+  text-transform: capitalize;
+  transition-duration: 176ms;
+
+  :hover {
+    color: #fe7777;
+  }
+`;
+
+const ColumnLink = styled.a`
+  font-size: 15px;
+  width: fit-content;
+  text-transform: capitalize;
+  transition-duration: 176ms;
+
+  :hover {
+    color: #fe7777;
+  }
+`;
+
+const StoreImage = styled.img`
+  width: 100px;
+`;
+
+export default ColumnsList;
