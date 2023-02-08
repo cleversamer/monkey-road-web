dfda4129177fe76e4370d21198168327f3db7792
cleversamerr <thedev.samer@gmail.com>
2023-02-08 18:07:21 +0200

    added https protocol to the server address

--- src/api/client.js
+++ src/api/client.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import cache from "utils/cache";
 
 const api = axios.create({
-  baseURL: "http://191.101.229.249/api",
+  baseURL: "https://191.101.229.249/api",
   // baseURL: "http://localhost:4000/api",
   headers: {
     "Content-Type": "application/json",
